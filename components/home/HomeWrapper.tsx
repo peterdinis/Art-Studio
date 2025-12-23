d2408bf5345108d176c41122badeca6fbc6cff66
peterdinis <petter1@pobox.sk>
2025-12-23 21:57:53 +0100

    feat: Working on layout WIP

--- components/home/HomeWrapper.tsx
+++ components/home/HomeWrapper.tsx
@@ -1,19 +1,33 @@
 import { FC } from "react";
 import { TooltipProvider } from "../ui/tooltip";
 import { TopMenuBar } from "../layout/TopMenuBar";
+import { ToolSidebar } from "../toolbar/ToolSidebar";
+import { BrushPanel } from "../panels/BrushPanel";
 
 const HomeWrapper: FC = () => {
     return (
-        <TooltipProvider delayDuration={200}>
-            <div className="h-screen w-full flex flex-col overflow-hidden bg-background">
-                <TopMenuBar />
-            </div>
+    <TooltipProvider delayDuration={200}>
+      <div className="h-screen w-full flex flex-col overflow-hidden bg-background">
+        {/* Top Menu Bar */}
+        <TopMenuBar />
 
-            <div className="flex-1 flex overflow-hidden">
-                TOOL SIDEBAR
-            </div>
-        </TooltipProvider>
-    )
+        {/* Main Content Area */}
+        <div className="flex-1 flex overflow-hidden">
+          {/* Left Tool Sidebar */}
+          <ToolSidebar />
+
+          CANVAS
+
+          {/* Right Panels */}
+          <div className="w-72 flex flex-col gap-2 p-2 overflow-y-auto scrollbar-thin">
+            <BrushPanel />
+          </div>
+        </div>
+
+        STATUS BAR
+      </div>
+    </TooltipProvider>
+  );
 }
 
 export default HomeWrapper;
