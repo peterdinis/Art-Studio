2f486deb84c7856aad8ba5ae60757e2a4083e50a
peterdinis <petter1@pobox.sk>
2025-12-29 19:07:27 +0100

    feat: Update next.config

--- next.config.ts
+++ next.config.ts
@@ -10,6 +10,7 @@ const nextConfig: NextConfig = {
 	typedRoutes: true,
 	experimental: {
 		optimizeCss: true,
+		optimizePackageImports: ["lucide-react", "framer-motion"],
 		browserDebugInfoInTerminal: true
 	},
 };
